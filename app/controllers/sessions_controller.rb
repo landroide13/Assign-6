85629122fb35b6d53768d477fb2ab697c580f80e
landroide13 <lazaro02@hotmail.com>
2017-08-12 15:30:59 +0700

    Almost

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-     
     @user = User.find_by(email: params[:email])
     if @user
       if @user.authenticate(params[:password])
