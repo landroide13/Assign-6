e3a97986fd44ed44b844f3cc5ed2be5f84df13f5
landroide13 <lazaro02@hotmail.com>
2017-08-18 11:28:06 +0700

    still likes

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,13 @@
+class LikesController < ApplicationController
+
+  before_action :authenticate_user! # devise method to help ensure logging in
+  
+  def toggle
+    if params[:post_id]
+      item = Post.find params[:post_id]
+    end
+    
+    current_user.toggle_like!(item)
+  end
+
+end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,14 +2,6 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :item, polymorphic: true
 
-  before_action :authenticate_user! # devise method to help ensure logging in
+  #before_action :authenticate_user! # devise method to help ensure logging in
   
- def toggle
-   if params[:post_id]
-     item = Post.find params[:post_id]
-   end
-   
-   current_user.toggle_like!(item)
- end
-
 end
